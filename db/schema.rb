d14cef946a79d554e0a8029a3a71d92a51d694a9
David Ma <davidma415@gmail.com>
2018-08-13 02:20:21 -0400

    Added quantity to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_13_060710) do
+ActiveRecord::Schema.define(version: 2018_08_13_061613) do
 
   create_table "parts", force: :cascade do |t|
     t.string "name"
     t.string "manufacturer"
     t.decimal "cost"
+    t.integer "quantity"
   end
 
 end
