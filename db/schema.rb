cf508330cc4a059186e32f7d37b2a27b3cb1f0cd
David Ma <davidma415@gmail.com>
2018-08-13 02:38:13 -0400

    Finished changing quantity to decimal

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_13_062605) do
+ActiveRecord::Schema.define(version: 2018_08_13_063641) do
+
+  create_table "locations", force: :cascade do |t|
+    t.string "address"
+    t.string "city"
+    t.string "province"
+    t.integer "sqft_size"
+    t.integer "capacity"
+    t.decimal "cost"
+  end
 
   create_table "parts", force: :cascade do |t|
     t.string "name"
     t.string "manufacturer"
     t.decimal "cost"
-    t.integer "quantity"
+    t.decimal "quantity"
   end
 
-  create_table "party_guests", force: :cascade do |t|
+  create_table "widgets", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "dietary_restrictions"
